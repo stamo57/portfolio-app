0a4cc8f3a2ea6f22d2f0798241110ad9b4406edb
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-11 16:04:08 -0500

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,4 +2,4 @@
 
 # Stop all servers and start the server as a daemon
 forever stopall
-forever start --minUptime 10000 --spinSleepTime 1000 /home/ec2-user/nodejs/app.js
+forever start --minUptime 10000 --spinSleepTime 1000 /home/ubuntu/nodejs/app.js
