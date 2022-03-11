c9c00e35ce083cb915f1987f4d3a04210943afea
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-11 15:48:02 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Install node.js  
-sudo yum install -y gcc-c++ make 
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-sudo yum update
-sudo yum install -y nodejs
+sudo apt-get update
+sudo apt-get install gcc g++ make
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs -y
+sudo npm install
+sudo apt install upstart -y
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+forever start --minUptime 10000 --spinSleepTime 1000 /home/ubuntu/git/storyBook/app.js
 
-# Install nodemon
-# sudo npm install nodemon -g
-
-# Install forever module 
-# https://www.npmjs.com/package/forever
-sudo npm install forever -g
 
