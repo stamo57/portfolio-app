22e086e164deb481d1e7b27ed5962c30d8b24d26
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-11 20:56:15 -0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #test
 #delete existing nodejs file if exist
-# if [ -d "/home/ubuntu/nodejs" ]; then rm -Rf /home/ubuntu/nodejs; fi
-# mkdir /home/ubuntu/nodejs
+if [ -d "/home/ubuntu/nodejs" ]; then rm -Rf /home/ubuntu/nodejs; fi
+mkdir /home/ubuntu/nodejs
 cd /home/ubuntu/nodejs
 
 sudo npm install
