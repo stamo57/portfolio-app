8cf50aac31611cc567c8e1432f0fe7d1b88f0c1c
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-12 06:57:09 -0600

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Stop all servers and start the server as a daemon
+# Stop all servers and start the server as a daemon 
 forever stopall
 sudo forever --sourceDir /home/ubuntu/myportfolio -c "npm start"  --minUptime 10000 --spinSleepTime 1000 /
