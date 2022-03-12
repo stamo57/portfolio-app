bb4648be0f5372383b710ff28607bfc62da34cb1
akinkunmi akindele hammed <72750823+akeenz@users.noreply.github.com>
2022-03-11 19:25:25 -0500

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,6 +9,5 @@ sudo npm install
 sudo apt install upstart -y
 pwd
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-#forever start --minUptime 10000 --spinSleepTime 1000 /home/ubuntu/git/storyBook/app.js
-forever start --minUptime 10000 --spinSleepTime 1000 /home/ubuntu/nodejs/portfolio-app/src/App.js
+
 
